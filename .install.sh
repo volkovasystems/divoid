73bdf5f4b22460af0f7cc08525e0ace6ffba917b
volkovasystems <richeve.bebedor@gmail.com>
2017-05-18 22:59:43 +0800

    Finished divoid procedure.

---
+++ .install.sh
@@ -0,0 +1,18 @@
+yarn add \
+babel-runtime \
+kein \
+mrkd \
+--save && \
+yarn add \
+babel-cli \
+babel-core \
+babel-plugin-transform-runtime \
+babel-preset-latest \
+source-map-loader \
+webpack \
+--dev && \
+bower install \
+kein \
+mrkd \
+--save \
+--force-latest
